94420ca9bb67de1954262049ebd8d9a685fc2416
Alper Şahıstan <alpersahistan@gmail.com>
2023-07-19 23:13:25 -0600

    Remove print

--- include/zfp/internal/array/view3.hpp
+++ include/zfp/internal/array/view3.hpp
@@ -535,9 +535,7 @@ protected:
   using preview<Container>::nz;
 
   // inspector
-  value_type get(size_t x, size_t y, size_t z) const {
-    printf("private_const_view::get(%d, %d, %d)\n", x, y, z);
-   return cache.get(x, y, z, this->stream); }
+  value_type get(size_t x, size_t y, size_t z) const {return cache.get(x, y, z, this->stream); }
 
   BlockCache3<value_type, store_type> cache; // cache of decompressed blocks
   void* stream = 0;// stream for compressed data
