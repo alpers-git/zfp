194cbd7e0647e6f1d5336eea10c1d6642c4c6bfc
alpers-git <alpersahistan@gmail.com>
2023-08-14 00:23:41 -0700

    prepare executable(to be removed) and cmake for sycl tests

---
+++ examples/syclTest.cpp
@@ -0,0 +1,18 @@
+#include <algorithm>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <iomanip>
+#include <iostream>
+#include <sstream>
+#include "zfp/array2.hpp"
+
+#include <CL/sycl.hpp>
+
+using namespace zfp;
+
+int main(int argc, char const *argv[])
+{
+    printf("zfp sycl test\n");
+    return 0;
+}
