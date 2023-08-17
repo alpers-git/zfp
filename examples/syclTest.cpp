6eb8f6460c3bc045aa834120e40c27cda3187627
alpers-git <alpersahistan@gmail.com>
2023-08-16 17:18:42 -0700

    Reorder sycl test to compare with serial and sycl imp.

--- examples/syclTest.cpp
+++ examples/syclTest.cpp
@@ -18,36 +18,61 @@ int main(int argc, char const *argv[])
 {
     printf("zfp sycl test\n");
 
-    const uint ny = 128;
-    const uint nx = 128;
-    const uint rate = 16;
+    const uint ny = 10;
+    const uint nx = 10;
+    uint rate = 16;
+    const zfp_type type = zfp_type_float;
+
     //create a c++ vector of linearly increasing values over nx*ny
     std::vector<float> v(nx*ny);
     std::iota(v.begin(), v.end(), 0);
 
-    zfp_field* field = zfp_field_2d(v.data(), zfp_type_float, nx, ny);
-    size_t blocks = zfp_field_blocks(field);
-    size_t scalars = blocks << (2 * 2);
-    size_t bufsize = 2 * scalars * sizeof(uint64);
-    void* buffer;
-    bitstream* stream;
-    zfp_stream* zfp;
-    buffer = new uint64[2 * scalars];
-    stream = stream_open(buffer, bufsize);
-    zfp = zfp_stream_open(stream);
+    //create a zfp field and set its type, pointer, and size for 2D
+    zfp_field* field = zfp_field_alloc();
+    zfp_field_set_type(field, type);
+    zfp_field_set_pointer(field, v.data());
+    zfp_field_set_size_2d(field, nx, ny);
+
+    //open a stream and allocate memory for compressed data
+    zfp_stream* zfp = zfp_stream_open(0);
+    rate = zfp_stream_set_rate(zfp, rate, type, 2, zfp_false);
+    size_t bufsize = zfp_stream_maximum_size(zfp, field);
+    uchar* buffer = new uchar[bufsize];
+    bitstream* s = stream_open(buffer, bufsize);
+    zfp_stream_set_bit_stream(zfp, s);
+
+    printf("=====Serial execution=====\n");
+
+    printf("----bitream before compression----\n");
+    //print the bitstream before compression
+    for (size_t i = 0; i < bufsize; i++)
+        printf("%02x", buffer[i]);
+    
+    zfp_stream_rewind(zfp);
+    size_t outsize = zfp_compress(zfp, field);
+    printf("\n----bitream after compression----\n");
+    //print the bitstream after compression
+    for (size_t i = 0; i < bufsize; i++)
+        printf("%02x", buffer[i]);
+
+    //reset and free the bitstream and zfp stream
+    zfp_stream_rewind(zfp);
+    stream_close(s);
+    zfp_stream_close(zfp);
+    delete[] buffer;
+
+    //create a new bitstream and zfp stream
+    buffer = new uchar[bufsize];
+    s = stream_open(buffer, bufsize);
+    zfp = zfp_stream_open(s);
+    zfp_stream_set_rate(zfp, rate, type, 2, zfp_false);
+    
+    //set execution mode to sycl
     if (!zfp_stream_set_execution(zfp, zfp_exec_sycl)) {
         fprintf(stderr, "execution not available\n");
         return EXIT_FAILURE;
     }
-    zfp_stream_set_rate(zfp, rate, zfp_field_type(field), zfp_field_dimensionality(field), zfp_false);
-    zfp_stream_rewind(zfp);
-    size_t zsize = zfp_compress(zfp, field);
-    //print the compressed size and the stream between begin and end
-    printf("zfp compressed size: %zu\n", zsize);
-    printf("zfp compressed stream: ");
-    for (size_t i = 0; i < zsize; i++) {
-        printf("%02x", ((uint8*)buffer)[i]);
-    }
+
 
     return 0;
 }
